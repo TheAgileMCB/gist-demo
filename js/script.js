789deec124474aee8e722a88cfaa4ef1c52ecbfa
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-02-24 13:21:18 -0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var firstName = prompt ('What\'s your first name?');
+
+console.log('First Name: ' + firstName);
+
+var middleName = prompt ('What\'s your middle name?');
+
+console.log('Middle Name: ' + middleName);
